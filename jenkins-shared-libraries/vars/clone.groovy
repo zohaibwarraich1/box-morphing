0d83307daac1ba20a55fb9245342ce891d799594
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2025-02-15 01:22:18 +0500

    Update clone.groovy

--- jenkins-shared-libraries/vars/clone.groovy
+++ jenkins-shared-libraries/vars/clone.groovy
@@ -1,15 +1,5 @@
-// def call(){
-//   echo "Cloning Project/repositery"
-//   sh "git clone https://github.com/zohaibwarraich1/box-morphing.git"
-//   echo "Cloned the project successfully"
-// }
-def call() {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: '*/main']],  // Apni branch ka naam confirm karein
-        userRemoteConfigs: [[
-            url: 'https://github.com/zohaibwarraich1/box-morphing.git', 
-            credentialsId: 'your-jenkins-credentials-id'
-        ]]
-    ])
+def call(){
+  echo "Cloning Project/repositery"
+  sh "git clone https://github.com/zohaibwarraich1/box-morphing.git"
+  echo "Cloned the project successfully"
 }
