a3606cf53b218ae999c39181e3f30b9d4df702f7
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2025-02-15 01:47:59 +0500

    Update docker_login.groovy

--- jenkins-shared-libraries/vars/docker_login.groovy
+++ jenkins-shared-libraries/vars/docker_login.groovy
@@ -1,6 +1,6 @@
 def call(){
   echo "logging into docker account"
-  withCredentials([usernamePassword(credentialsId: 'docker_login', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+  withCredentials([usernamePassword(credentialsId: "docker-login-zohaibwarraich", usernameVariable: "USER", passwordVariable: "PASS")]){
     sh "docker login -u ${env.USER} -p ${env.PASS}"
     echo "Successfully! logged into ${env.USER}"
   }
