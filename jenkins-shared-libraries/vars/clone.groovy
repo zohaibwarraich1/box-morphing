0c87e2adfc3907b2981b9f5f1d623596b022cca1
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2025-02-14 23:57:11 +0500

    Update clone.groovy

--- jenkins-shared-libraries/vars/clone.groovy
+++ jenkins-shared-libraries/vars/clone.groovy
@@ -1,5 +1,5 @@
 def clone(){
   echo "Cloning Project/repositery"
-  sh "git clone https://github.com/zohaibwarraich1/box-morphing"
+  sh "git clone https://github.com/zohaibwarraich1/box-morphing.git"
   echo "Cloned the project successfully"
 }
