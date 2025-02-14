5537f8e64d14f0ddb19b19c8377ba08419d4e5e0
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2025-02-15 01:06:03 +0500

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(){
   echo "Cloning Project/repositery"
-  git url: "git clone https://github.com/zohaibwarraich1/box-morphing.git", branch: "main"
+  git url: "https://github.com/zohaibwarraich1/box-morphing.git", branch: "main"
   echo "Cloned the project successfully"
 }
